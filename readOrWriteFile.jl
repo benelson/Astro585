481d85c549bdd402e95b5473b2ede04eaaba7dcb
benelson <benelson7@gmail.com>
2014-03-04 22:51:43 -0500

    fixed read/write to binary functions

--- readOrWriteFile.jl
+++ readOrWriteFile.jl
@@ -7,11 +7,15 @@ function read_array_from_file(filepath::String)
     @elapsed x = readdlm(filepath)
 end
 
-function write_array_to_file(x::Array)
-    try rm("outfile.out") catch end
-    @elapsed writedlm("outfile.out",x)
+function write_array_to_binary(x::Array)
+    try rm("outfile.bin") catch end
+    @elapsed open("outfile.bin","w") do f
+        serialize(f, x)
+    end
 end
 
-function read_array_from_file(filepath::String)
-    @elapsed x = readdlm(filepath)
+function read_array_from_binary(filepath::String)
+    @elapsed open(filepath,"r") do f
+        x = deserialize(f)
+    end
 end
