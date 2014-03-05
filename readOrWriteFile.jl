71a53510b9a304169d850af2d69a319c8bbce393
benelson <benelson7@gmail.com>
2014-03-04 22:33:30 -0500

    modified to read/write to binary

--- readOrWriteFile.jl
+++ readOrWriteFile.jl
@@ -6,3 +6,16 @@ end
 function read_array_from_file(filepath::String)
     @elapsed x = readdlm(filepath)
 end
+
+function write_array_to_binary(x::Array)
+    try rm("outfile.bin") catch end
+    @elapsed open("outfile.bin","w") do f
+        serialize(f, x)
+    end
+end
+
+function read_array_from_binary(filepath::String)
+    @elapsed open(filepath,"w") do f
+        x = deserialize(f)
+    end
+end
