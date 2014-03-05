a9d708e5eb1b5ecf53afb3b760edb96c84449a2b
benelson <benelson7@gmail.com>
2014-03-04 22:39:26 -0500

    fix bug in binary functions

--- readOrWriteFile.jl
+++ readOrWriteFile.jl
@@ -7,15 +7,11 @@ function read_array_from_file(filepath::String)
     @elapsed x = readdlm(filepath)
 end
 
-function write_array_to_binary(x::Array)
-    try rm("outfile.bin") catch end
-    @elapsed open("outfile.bin","w") do f
-        serialize(f, x)
-    end
+function write_array_to_file(x::Array)
+    try rm("outfile.out") catch end
+    @elapsed writedlm("outfile.out",x)
 end
 
-function read_array_from_binary(filepath::String)
-    @elapsed open(filepath,"w") do f
-        x = deserialize(f)
-    end
+function read_array_from_file(filepath::String)
+    @elapsed x = readdlm(filepath)
 end
