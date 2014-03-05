0f1397ee0bd2a008b8e842ebc54d630af4d3dd77
benelson <benelson7@gmail.com>
2014-03-04 22:17:30 -0500

    added function to read or write to file

---
+++ readOrWriteFile.jl
@@ -0,0 +1,8 @@
+function write_array_to_file(x::Array)
+    try rm("outfile.out") catch end
+    @elapsed writedlm("outfile.out",x)
+end
+
+function read_array_from_file(filepath::String)
+    @elapsed x = readdlm(filepath)
+end
